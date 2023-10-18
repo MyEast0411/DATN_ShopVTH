f02b5a7e3bb335f5bf6885ceddfae51da2785e6e
DESKTOP-5ENQ66J\ASUS <dongqdph29107@fpt.edu.vn>
2023-10-19 00:08:45 +0700

    commit linh tinh

--- FE-admin/src/pages/NhanVien.jsx
+++ FE-admin/src/pages/NhanVien.jsx
@@ -1,43 +1,9 @@
 import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
-
+import TableAllSanPham from '../common/tableNextUi/sanPham/TableAllSanPham'
 export default function NhanVien() {
-  const [showScanner, setShowScanner] = useState(false); // Biến trạng thái để kiểm soát việc hiển thị chỗ quét mã
-
-  const handleStartScanning = () => {
-    setShowScanner(true);
-  }
-
-  const handleEndScanning = () => {
-    setShowScanner(false);
-  }
-
-  const handleScan = (data) => {
-    console.log(data);
-    if (data!=undefined) {
-      // Xử lý kết quả quét ở đây
-      handleEndScanning();
-    }
-  }
-
-  const handleError = (error) => {
-    console.error(error);
-  }
-
   return (  
     <div>
-      <p>Đây là giao diện quản lý nhân viên</p>
-      {showScanner ? ( 
-        <div>
-          <QrReader
-            onResult={handleScan}
-            onError={handleError}
-            style={{ width: '100%' }}
-          />
-        </div>
-      ) : (
-        <button onClick={handleStartScanning}>Bắt đầu quét mã</button>
-      )}
+      <TableAllSanPham />
     </div>
   );
 }
