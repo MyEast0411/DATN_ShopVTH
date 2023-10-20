bf5bdcf8db326ae0d7b11ce59c3015c85ec2f812
Fest <100851969+NguyenHoi@users.noreply.github.com>
2023-10-20 22:32:23 +0700

    commit

--- BE/src/main/java/com/example/shop/components/KhuyenMaiScheduler.java
+++ BE/src/main/java/com/example/shop/components/KhuyenMaiScheduler.java
@@ -5,6 +5,7 @@ import com.example.shop.services.KhuyenMaiService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -21,25 +22,30 @@ public class KhuyenMaiScheduler {
         Date currentDate = new Date();
         List<KhuyenMai> khuyenMaiList = khuyenMaiService.findAll();
         for (KhuyenMai khuyenMai : khuyenMaiList) {
-            if (khuyenMai.getNgayBatDau().after(currentDate)) {
-                khuyenMai.setTrangThai("Sắp diễn ra");
-                System.out.println("Khuyen Mai name: " + khuyenMai.getTen());
-                System.out.println("Current date: " + currentDate);
-                System.out.println("Start date: " + khuyenMai.getNgayBatDau());
-                System.out.println("End date: " + khuyenMai.getNgayKetThuc());
-                System.out.println(khuyenMai.getTrangThai());
-            } else if (khuyenMai.getNgayKetThuc().before(currentDate)) {
-                khuyenMai.setTrangThai("Đã kết thúc");
-                System.out.println("Khuyen Mai name: " + khuyenMai.getTen());
-                System.out.println("Current date: " + currentDate);
-                System.out.println("Start date: " + khuyenMai.getNgayBatDau());
-                System.out.println("End date: " + khuyenMai.getNgayKetThuc());
-                System.out.println(khuyenMai.getTrangThai());
-            } else {
-                khuyenMai.setTrangThai("Đang diễn ra");
-                System.out.println("Active");
+            if (khuyenMai.getTen().equals("Test")) {
+                if (khuyenMai.getNgayBatDau().after(currentDate)) {
+                    khuyenMai.setTrangThai("Sắp diễn ra");
+                    System.out.println("Khuyen Mai name: " + khuyenMai.getTen());
+                    System.out.println("Current date: " + currentDate);
+                    System.out.println("Start date: " + khuyenMai.getNgayBatDau());
+                    System.out.println("End date: " + khuyenMai.getNgayKetThuc());
+                    System.out.println(khuyenMai.getTrangThai());
+                } else if (khuyenMai.getNgayKetThuc().before(currentDate)) {
+                    khuyenMai.setTrangThai("Đã kết thúc");
+                    System.out.println("Khuyen Mai name: " + khuyenMai.getTen());
+                    System.out.println("Current date: " + currentDate);
+                    System.out.println("Start date: " + khuyenMai.getNgayBatDau());
+                    System.out.println("End date: " + khuyenMai.getNgayKetThuc());
+                    System.out.println(khuyenMai.getTrangThai());
+                } else {
+                    khuyenMai.setTrangThai("Đang diễn ra");
+                    System.out.println("Khuyen Mai name: " + khuyenMai.getTen());
+                    System.out.println("Current date: " + currentDate);
+                    System.out.println("Start date: " + khuyenMai.getNgayBatDau());
+                    System.out.println("End date: " + khuyenMai.getNgayKetThuc());
+                    System.out.println(khuyenMai.getTrangThai());
+                }
             }
-
             khuyenMaiService.save(khuyenMai);
         }
     }
