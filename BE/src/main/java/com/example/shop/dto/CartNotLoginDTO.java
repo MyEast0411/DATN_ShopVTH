ee77a33e403619c2b51aacd3ffcb56add2ccc985
Hội Nguyễn Văn <nguyenvanhoi2k3@gmail.com>
2024-01-09 23:33:45 +0700

    hoi commit

---
+++ BE/src/main/java/com/example/shop/dto/CartNotLoginDTO.java
@@ -0,0 +1,27 @@
+package com.example.shop.dto;
+
+import com.example.shop.entity.SanPhamChiTiet;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CartNotLoginDTO {
+    private String email;
+    private String hoTen;
+    private String soDienThoai;
+    private String diaChi;
+    private String thanhPho;
+    private String quanHuyen;
+    private String xaPhuong;
+    private String phuongThucThanhToan;
+    private String deliveryTime;
+    private String phiVanChuyen;
+    private List<SanPhamChiTiet> sanPhams;
+    private String tongTien;
+}
