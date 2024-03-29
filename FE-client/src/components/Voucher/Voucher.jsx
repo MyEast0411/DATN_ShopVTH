e3ab37294ead5e503aede35ebffc2a79b993df0f
MyEast0411 <dongqdph29107@fpt.edu.vn>
2024-03-29 15:01:30 +0700

    update load voucher cua khach hang - voucher cong khai

--- FE-client/src/components/Voucher/Voucher.jsx
+++ FE-client/src/components/Voucher/Voucher.jsx
@@ -3,15 +3,16 @@ import Header from "../../layout/Header";
 import Footer from "../../layout/Footer";
 import InfoTop from "../../layout/InfoTop";
 import { Link } from "react-router-dom";
-import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
+import { Breadcrumbs, BreadcrumbItem, user } from "@nextui-org/react";
 import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 import CouponPrivate from "./CouponPrivate";
 import CouponPublic from "./CouponPublic";
 import axios from "axios";
+import { format } from 'date-fns';
 
 export default function Voucher() {
   const [coupons, setCoupons] = useState([]);
-
+  const [activeTab, setActiveTab] = useState('Công khai');
   const initialCoupons = [
     {
       id: "voucher001",
@@ -31,23 +32,52 @@ export default function Voucher() {
 
   const getVoucherByIdKhachHang = async () => {
     await axios.get(`http://localhost:8080/voucher/getVoucherByIdKhachHang/${JSON.parse(localStorage.getItem("user"))?.id}`).then((response) => {
+      setCoupons([]);
       response.data.map((item) => {
-        setCoupons({
+        console.log(response.data);
+        const newCoupon = {
           id: item.idVoucher,
           percentOff: item.tenVoucher,
-          maxValue: item.giaTriMax,
-          minValue: item.giaTriMin,
-          expirationDate: item.ngayKetThuc
-        });
-      })
-      console.log(coupons);
+          maxValue: Intl.NumberFormat().format(item.giaTriMax),
+          minValue: Intl.NumberFormat().format( item.giaTriMin),
+          expirationDate:  format(item.ngayKetThuc, 'dd-MM-yyyy') 
+        };
+        setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+      });
     })
   }
+  const getVocherDuocDung = async () => {
+    await axios.get("http://localhost:8080/voucher/getVouchers").then((response) => {
+      setCoupons([]);
+      response.data.map((item) => {
+        const newCoupon = {
+          id: item.id,
+          percentOff: item.code,
+          maxValue: Intl.NumberFormat().format(item.giaTriMax),
+          minValue: Intl.NumberFormat().format( item.giaTriMin),
+          expirationDate:  format(item.ngayKetThuc, 'dd-MM-yyyy') 
+        };
+        setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+      });
+    }).catch((err) => { 
+      console.log(err);
+    });
+  };
   useEffect(() => {
-    getVoucherByIdKhachHang();
+    // getVoucherByIdKhachHang();
     // setCoupons(initialCoupons);
+    getVocherDuocDung();
   }, []);
-
+  const toggleTab = (tab) => {
+    if (activeTab !== tab) {
+      setActiveTab(tab);
+      if (tab == 'Công khai') {
+        getVocherDuocDung();
+      } else if (tab == 'Cá nhân') {
+        getVoucherByIdKhachHang();
+      }
+    }
+  }
   return (
     <>
       <InfoTop />
@@ -70,13 +100,16 @@ export default function Voucher() {
         </div>
 
         <div className="flex w-full flex-col ">
-          <Tabs aria-label="Options">
+          <Tabs aria-label="Options" onClick={(e) => {
+            console.log(e.target.innerText);
+             toggleTab(e.target.innerText)
+            }}>
             <Tab key="public" title="Công khai">
               <Card>
                 <CardBody>
                   {" "}
-                  <div className="coupon-item  min-h-[400px]">
-                    {coupons.length > 1 && coupons.map((coupon, index) => (
+                  <div className="coupon-item  min-h-[400px]" >
+                    {coupons.length >= 1 && coupons.map((coupon, index) => (
                       <CouponPublic key={index} {...coupon} />
                     ))}
                   </div>
@@ -87,7 +120,7 @@ export default function Voucher() {
               <Card>
                 <CardBody>
                   <div className="coupon-item min-h-[400px]">
-                    {coupons.length > 1 && coupons.map((coupon, index) => (
+                    {coupons.length >= 1 && coupons.map((coupon, index) => (
                       <CouponPrivate key={index} {...coupon} />
                     ))}
                   </div>
